3f88bc26c0373a67ff31d65adf3dbc5e3453e921
Rohit Jain <jain.roh@husky.neu.edu>
2019-03-23 04:22:40 -0400

    Change in Data

--- MlFlow.py
+++ MlFlow.py
@@ -4,7 +4,6 @@
 import mlflow
 import os
 import sys
-import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     a=int(sys.argv[1])
@@ -14,12 +13,7 @@ if __name__ == "__main__":
     c=a*b
     print("Multiplication :"+str(c))
     mlflow.log_metric("mutiplication",c)
-    plt.plot(a, b)
-    plt.xlabel('A')
-    plt.ylabel('B')
-    plt.title('Mutiplication Values')
-    plt.savefig('info.png')
-    mlflow.log_artifact("info.png")
+
 
 
 
