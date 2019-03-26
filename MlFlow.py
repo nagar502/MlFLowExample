f9df0eb7eeaacc30604e82b4de2d4923835caca4
jain-roh <34594228+jain-roh@users.noreply.github.com>
2019-03-26 01:29:56 -0400

    Update MlFlow.py

--- MlFlow.py
+++ MlFlow.py
@@ -1,10 +1,9 @@
-
-
-
 import mlflow
 import os
 import sys
 import mlflow.sklearn
+import matplotlib.pyplot as plt
+import os 
 
 if __name__ == "__main__":
     a=int(sys.argv[1])
@@ -13,15 +12,8 @@ if __name__ == "__main__":
     mlflow.log_param("val2",b)
     c=a*b
     print("Multiplication :"+str(c))
+    cwd = os.getcwd()    
+    plt.plot(5, 10,'go--', linewidth=2, markersize=12)
+    plt.savefig(cwd+'/foo.png')
     mlflow.log_metric("mutiplication",c)
     mlflow.sklearn.log_model(c, "model")
-
-
-
-
-
-
-
-
-
-
